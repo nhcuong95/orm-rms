406c8b4fbc4ef509d58589dc325a8740b88dafbd
orm-resource-manager <75389324+orm-resource-manager@users.noreply.github.com>
2020-12-03 13:43:31 -0800

    Update main.tf
    Added vcn to main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,11 @@ resource "null_resource" "git_backend" {
     command = "echo \"testing git backend from Github private repo\""
   }
 }
+
+resource "oci_core_virtual_network" "vcn1" {
+  cidr_block = "10.0.0.0/16"
+  dns_label = "vcn1"
+  compartment_id = "${var.compartment_ocid}"
+  display_name = "${var.display_name_prefix}VCN1-1 for maestro-integ-tests"
+}
+
