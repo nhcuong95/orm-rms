430a54ce544ea71c19a10542c0b03725b49503f9
orm-resource-manager <75389324+orm-resource-manager@users.noreply.github.com>
2020-12-03 13:44:05 -0800

    updated main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ variable "display_name_prefix" {}
 
 resource "null_resource" "git_backend" {
   provisioner "local-exec" {
-    command = "echo \"testing git backend from Github private repo\""
+    command = "echo \"testing git backend from Github private repo.....updated!\""
   }
 }
